229bf995cc0a79c0104789c0d6278791caf31f5b
Lili Sazonova <lilissazonova@gmail.com>
2024-12-18 16:17:36 +0200

    fix

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://siren-store.vercel.app';
 
 const api = axios.create({
   baseURL: API_URL,
